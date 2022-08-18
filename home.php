2ce518394c48062a3b01d9a42eedebaab5d9a2d9
Lakmal-Home <go2lakmal@gmil.com>
2022-08-19 01:19:18 +0530

    1.1

--- home.php
+++ home.php
@@ -1,67 +1,110 @@
 <?php
 session_start();
 include("conn.php");
- 
 
-if(isset($_SESSION['username'])) {
-    $user=$_SESSION['username'];
-}else{
-    $user="Login";
+
+if (isset($_SESSION['username'])) {
+  $user = $_SESSION['username'];
+} else {
+  $user = "Login";
 }
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Hardware System - Workshop</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 
-<style>
-    .divback{
-    
-    text-align: left;
-    font-size: 25px;
-    font-weight: 500;
-}
-</style>
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+  <style>
+    .divback {
+      align-items: left;
+      text-align: left;
+      font-size: 25px;
+      font-weight: 500;
+    }
+
+    .back-color {
+      background-color: black;
+    }
+  </style>
 
 </head>
+
 <body>
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-black">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </div>
+        </div>
+      </div>
+    </nav>
 
-<nav class="navbar navbar-inverse divback">
-  <div class="container-fluid ">
-    <div class="navbar-header">
-      <a class="navbar-brand divback" href="home.php">HWS</a>
-    </div>
-    <ul class="nav navbar-nav">
-      
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Workshop<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="newWorkshop.php">New Workshop</a></li>
-          <li><a href="#">Previous Workshops</a></li>
-          <li><a href="#">Advanced Editings</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Page 2</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-        
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> <?php echo $user ?></a></li>
-    </ul>
   </div>
-</nav>
-  
-<div class="container">
-  <h3>Right Aligned Navbar</h3>
-  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
-</div>
 
+
+
+  <div class="container">
+    <h2>Cards Columns</h2>
+    <p>The .card-columns class creates a masonry-like grid of cards. The layout will automatically adjust as you insert more cards.</p>
+    <p><strong>Note:</strong> The cards are displayed vertically on small screens (less than 576px):</p>
+    <div class="card-columns">
+      <div class="card bg-primary">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the first card</p>
+        </div>
+      </div>
+      <div class="card bg-warning">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the second card</p>
+        </div>
+      </div>
+      <div class="card bg-success">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the third card</p>
+        </div>
+      </div>
+      <div class="card bg-danger">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the fourth card</p>
+        </div>
+      </div>
+      <div class="card bg-light">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the fifth card</p>
+        </div>
+      </div>
+      <div class="card bg-info">
+        <div class="card-body text-center">
+          <p class="card-text">Some text inside the sixth card</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
